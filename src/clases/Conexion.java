13f917a2c94b268ee9a8cb280d73f4532f6ea92c
luiso-o <cheportillo@gmail.com>
2023-05-20 11:41:55 +0200

    primer commit

---
+++ src/clases/Conexion.java
@@ -0,0 +1,18 @@
+package clases;
+import java.sql.*;
+
+public class Conexion {
+    //Conexion local
+    public static Connection conectar(){
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_ds","root","");
+            return cn;
+            
+        } catch (SQLException e) {
+            System.out.println("Error en la conexion local\n -revisa pakcage clases/Conexion" + e);
+            
+        }
+        return (null);
+        
+    }
+}
